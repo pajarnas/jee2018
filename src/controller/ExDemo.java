5d217443ee9f26ff70b52d1ec8c17e9fd23e5c3b
ljcyu <ljcyu@163.com>
2018-04-19 06:46:33 +0800

    增加随机抛出异常，action处理和全局处理

--- src/controller/ExDemo.java
+++ src/controller/ExDemo.java
@@ -3,12 +3,19 @@ package controller;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import org.slf4j.*;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 public class ExDemo extends ActionSupport {
+  final static Logger logger=LoggerFactory.getLogger(ExDemo.class);
   @Override public String execute()throws Exception{
     double d=Math.random();
-    if(d>0.5) throw new IOException("io ex");
+    if(d>0.5){
+      Exception ex=new IOException("io ex");
+      logger.error(ex.getMessage());      
+      throw ex;
+    }
     else throw new SQLException("sql ex");
   }
 }
